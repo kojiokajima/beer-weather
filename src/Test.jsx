4b8ad29e2700f990047154e140d3d0ebb24787b5
wanko <kouji124@w4.dion.ne.jp>
2021-03-23 14:24:49 -0700

    created datafiles

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import BeerData from './beerData'
 
 const Test = () => {
-    const [data, setData] = useState()
+  const [beerData, setBeerData] = useState([])
 
   useEffect(() => {
+    const temp = 27
+    const weather = 'sunny'
+    // こんなやつ --> beerData.cold.sunny[0]のなかに[name, style, alc, IBU, description]
+    // if (temp )
+
+
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`;
-    const beerUrl = `https://sandbox-api.brewerydb.com/v2/styles/?key=${process.env.REACT_APP_BEER_API}`;
-    console.log(beerUrl)
-    axios.get(beerUrl).then((response) => {
-      console.log(response);
-    });
+    // const beerUrl = `https://sandbox-api.brewerydb.com/v2/styles/?key=${process.env.REACT_APP_BEER_API}`;
+    // console.log(beerUrl)
+    // axios.get(beerUrl).then((response) => {
+    //   console.log(response);
+    // });
+
+    // fetch(beerUrl, {mode: 'cors'})
+    // .then((response) => {
+    //   response.json()
+    // })
+    // .then((data) => {
+    //   console.log(data)
+    // })
   }, []);
+
+  // switch()
+
   return <div>Yoo</div>;
 };
 
---
+++ src/beerData.js
@@ -0,0 +1,92 @@
+export const beerData = {
+    cold: {
+        sunny: [
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], // --> beerData.cold.sunny[0]
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], // --> beerData.cold.sunny[1]
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+        ],
+        cloudy: [
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+        ],
+        rainy: [
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+            ['1772 Porter', 'Brown Porter', '4.6%', 26, 'coldcoldcold'], 
+        ]
+
+    },
+
+    coldModerate: {
+        sunny: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[0]
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[1]
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ],
+        cloudy: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ],
+        rainy: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ]
+    },
+
+    moderate: {
+        sunny: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[0]
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[1]
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ],
+        cloudy: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ],
+        rainy: [
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+        ]
+    },
+
+    moderateWarm: {
+        sunny: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[0]
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[1]
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ],
+        cloudy: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ],
+        rainy: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ]
+    },
+
+    warm: {
+        sunny: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[0]
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[1]
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ],
+        cloudy: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ],
+        rainy: [
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+            ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
+        ]
+    }
+}
