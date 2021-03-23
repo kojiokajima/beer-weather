728dbb2961e839791705f1aabd1d825aeb8c1ec1
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-23 14:58:46 -0700

    added time weather part and week weather part

--- src/Inae.js
+++ src/Inae.js
@@ -2,6 +2,7 @@ import './sass/main.scss';
 import Logo from './sass/img/fake_logo.png';
 import icon from './sass/img/icon.png';
 import Beer from './sass/img/coronas.png';
+import BeerIcon from './sass/img/beer_w.png'
 
 function Inae() {
   return (
@@ -59,6 +60,144 @@ function Inae() {
             </div>
 
         </div>
+        <hr></hr>
+        <div className="weather-time">
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+            <div className="weather-content">
+                <h3 className="time-text">Now</h3>
+                <img src={icon} className="weather-content-icon" />
+                <h3 className="temp-text">24°</h3>
+            </div>
+        </div>
+        <hr></hr>
+
+        <div className="weather-week">
+            <div className="week-content">
+                <h3 className="time-text">Tuesday</h3>
+                <img src={icon} className="weather-content-icon" />
+                <div className="beer-reco">
+                    <img src={BeerIcon} className="beer-content-icon" />
+                    <h3 className="beer-content-text">Drink Outside</h3>
+                </div>
+                <h3 className="temp-text">24°</h3>
+                <div className="temp-low-high">
+                    <h3 className="temp-text high">24°</h3>
+                    <h3 className="temp-text low">14°</h3>
+                </div>
+            </div>
+            <div className="week-content">
+                <h3 className="time-text">Tuesday</h3>
+                <img src={icon} className="weather-content-icon" />
+                <div className="beer-reco">
+                    <img src={BeerIcon} className="beer-content-icon" />
+                    <h3 className="beer-content-text">Drink Outside</h3>
+                </div>
+                <h3 className="temp-text">24°</h3>
+                <div className="temp-low-high">
+                    <h3 className="temp-text high">24°</h3>
+                    <h3 className="temp-text low">14°</h3>
+                </div>
+            </div>
+            <div className="week-content">
+                <h3 className="time-text">Tuesday</h3>
+                <img src={icon} className="weather-content-icon" />
+                <div className="beer-reco">
+                    <img src={BeerIcon} className="beer-content-icon" />
+                    <h3 className="beer-content-text">Drink Outside</h3>
+                </div>
+                <h3 className="temp-text">24°</h3>
+                <div className="temp-low-high">
+                    <h3 className="temp-text high">24°</h3>
+                    <h3 className="temp-text low">14°</h3>
+                </div>
+            </div>
+            <div className="week-content">
+                <h3 className="time-text">Tuesday</h3>
+                <img src={icon} className="weather-content-icon" />
+                <div className="beer-reco">
+                    <img src={BeerIcon} className="beer-content-icon" />
+                    <h3 className="beer-content-text">Drink Outside</h3>
+                </div>
+                <h3 className="temp-text">24°</h3>
+                <div className="temp-low-high">
+                    <h3 className="temp-text high">24°</h3>
+                    <h3 className="temp-text low">14°</h3>
+                </div>
+            </div>
+            <div className="week-content">
+                <h3 className="time-text">Tuesday</h3>
+                <img src={icon} className="weather-content-icon" />
+                <div className="beer-reco">
+                    <img src={BeerIcon} className="beer-content-icon" />
+                    <h3 className="beer-content-text">Drink Outside</h3>
+                </div>
+                <h3 className="temp-text">24°</h3>
+                <div className="temp-low-high">
+                    <h3 className="temp-text high">24°</h3>
+                    <h3 className="temp-text low">14°</h3>
+                </div>
+            </div>
+        </div>
+        
         <div className="beer-wave-all">
             <div className="beer-waves">            
             </div>
