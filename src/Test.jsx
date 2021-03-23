cdda99a4d6e2875412952bfa9bf059debe05fd1e
wanko <kouji124@w4.dion.ne.jp>
2021-03-23 15:02:28 -0700

    set up temporary data

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,15 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import BeerData from './beerData'
+import {beerData} from './beerData'
 
 const Test = () => {
-  const [beerData, setBeerData] = useState([])
+  const [data, setData] = useState(beerData.cold.sunny)
+  const temp = 27
+  const weather = 'sunny'
+  const random =  Math.floor(Math.random() * 3)
 
   useEffect(() => {
-    const temp = 27
-    const weather = 'sunny'
+
+    if (temp < 15) {
+      switch(weather){
+        case 'sunny':
+          setData(beerData.cold.sunny)
+          break;
+        case 'cloudy':
+          setData(beerData.cold.cloudy)
+          break;
+        case 'rainy':
+          setData(beerData.cold.rainy)
+          break;
+      }
+    } else if (temp >= 15 && temp <= 25) {
+      switch(weather){
+        case 'sunny':
+          setData(beerData.moderate.sunny)
+          break;
+        case 'cloudy':
+          setData(beerData.moderate.cloudy)
+          break;
+        case 'rainy':
+          setData(beerData.moderate.rainy)
+          break;
+      }
+    } else if (temp > 25) {
+      switch(weather){
+        case 'sunny':
+          setData(beerData.warm.sunny)
+          break;
+        case 'cloudy':
+          setData(beerData.warm.cloudy)
+          break;
+        case 'rainy':
+          setData(beerData.warm.rainy)
+          break;
+      }
+    }
+
+
     // こんなやつ --> beerData.cold.sunny[0]のなかに[name, style, alc, IBU, description]
-    // if (temp )
+    // if (temp)
 
 
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`;
@@ -29,8 +70,23 @@ const Test = () => {
   }, []);
 
   // switch()
+  console.log(data)
+  console.log(random)
+  console.log(data[random])
+  console.log(data[random][0])
+  // console.log(typeof(data[random]))
+  // console.log(...data[random])
+  // console.log(data[random][0])
 
-  return <div>Yoo</div>;
+  return (
+    <div>
+      <p>name: {data[random][0]}</p>
+      <p>style: {data[random][1]}</p>
+      <p>Alc: {data[random][2]}</p>
+      <p>IBU: {data[random][3]}</p>
+      <p>Description: {data[random][4]}</p>
+    </div>
+  );
 };
 
 export default Test;
--- src/beerData.js
+++ src/beerData.js
@@ -20,19 +20,25 @@ export const beerData = {
 
     coldModerate: {
         sunny: [
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[0]
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'], // --> beerData.moderate.sunny[1]
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            // ...beerData.cold.sunny,
+            // ...beerData.moderate.sunny,
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'], // --> beerData.moderate.sunny[0]
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'], // --> beerData.moderate.sunny[1]
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
         ],
         cloudy: [
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            // ...beerData.cold.cloudy,
+            // ...beerData.moderate.cloudy,
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
         ],
         rainy: [
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
-            ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
+            // ...beerData.cold.rainy,
+            // ...beerData.moderate.rainy,
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
+            ['Billy Bock', 'Bock', '6.7%', 23, 'coldModerate'],
         ]
     },
 
@@ -53,19 +59,25 @@ export const beerData = {
             ['Alferd Packer Killer Kölsch', 'Kölsch', '5.1%', 25, 'moderatemoderate'],
         ]
     },
-
+    
     moderateWarm: {
         sunny: [
+            // ...beerData.moderate.sunny,
+            // ...beerData.warm.sunny,
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[0]
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'], // --> beerData.warm.sunny[1]
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
         ],
         cloudy: [
+            // ...beerData.moderate.cloud,
+            // ...beerData.warm.cloud,
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
         ],
         rainy: [
+            // ...beerData.moderate.rainy,
+            // ...beerData.warm.rainy,
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
             ['Barn Phantom Gose with Mango', 'Gose', '4%', 10, 'wormwormwormworm'],
