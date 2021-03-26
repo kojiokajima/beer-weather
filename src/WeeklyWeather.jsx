10d29b6c4092710e3f234bbd68b7de1ccf939755
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-26 00:45:57 -0700

    fixed api return  error inside weekly weather

--- src/WeeklyWeather.jsx
+++ src/WeeklyWeather.jsx
@@ -11,42 +11,49 @@ const WeeklyWeather = (text)=>{
       })
 
     useEffect(()=>{
-        getWeeklyWeather(text)
-        // getWeather(text)
+        // getWeeklyWeather(text)
+        getWeather(text)
     },[text])
 
-    // async function getWeather(text){
-    //     const result = await axios.get(
-    //             `https://api.openweathermap.org/data/2.5/forecast?q=${text.text}&appid=${process.env.REACT_APP_WEATHER_API}`
-    //         ).then(resposeData => {
-    //         console.log(resposeData.data);
-    //         const dailyData = resposeData.data.list.filter(list => list.dt_txt.includes("18:00:00"))
-    //         setState(dailyData)
-    //         // setState({
-    //         //     fullData: resposeData.data.list,
-    //         //     dailyData: dailyData
-    //         //   }, () => console.log(state))
-    //         // }).catch(error => console.log(error))
-    //         })
-    // } 
+    async function getWeather(){
+        const result = await axios.get(
+                `https://api.openweathermap.org/data/2.5/forecast?q=${text.text}&appid=${process.env.REACT_APP_WEATHER_API}`
+            ).then(resposeData => {
+            console.log(resposeData.data);
+            const dailyData = resposeData.data.list.filter(list => list.dt_txt.includes("18:00:00"))
+            // setState(dailyData)
+            setState({
+                fullData: resposeData.data.list,
+                dailyData: dailyData
+              })
+              console.log(state)
+            }).catch(error => console.log(error))
+            // })
+    } 
 
   
-    const getWeeklyWeather = () => {
-      const weatherURL =
-      // `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${process.env.REACT_APP_WEATHER_API}`
-      `https://api.openweathermap.org/data/2.5/forecast?q=${text.text}&appid=${process.env.REACT_APP_WEATHER_API}`
+    // const getWeeklyWeather = () => {
+    //   const weatherURL =
+    //   // `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${process.env.REACT_APP_WEATHER_API}`
+    //   `https://api.openweathermap.org/data/2.5/forecast?q=${text.text}&appid=${process.env.REACT_APP_WEATHER_API}`
   
-      fetch(weatherURL)
-      .then(res => res.json())
-      .then(data => {
-        const dailyData = data.list.filter(list => list.dt_txt.includes("18:00:00"))
+    //   fetch(weatherURL)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     const dailyData = data.list.filter(list => list.dt_txt.includes("18:00:00"))
 
-        setState({
-          fullData: data.list,
-          dailyData: dailyData
-        })
-        console.log(state)
-      })
+    //     setState({
+    //       fullData: data.list,
+    //       dailyData: dailyData
+    //     })
+    //     console.log(state)
+    //   })
+    // }
+
+    if(!text.text){
+        return(
+            <h1>Loading</h1>
+        )
     }
   
   
